1845304b3c0d104306ec58f79a069415e1e60258
UndeadBigUnicorn <kravchel16@gmail.com>
2020-03-07 21:28:48 +0200

    add tables rendering and value validation

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,27 +1,59 @@
 $(document).ready(() => {
 
     const criteria = ["Ціна", "Запас ходу", "Швидкість", "Зарядки", "Вмісткість", "Комфорт"];
-    const criteriaList = $('#criteria-list');
+    const cars = ["Tesla Model 3", "Nissan Leaf", "VW e-Golf", "Hyundai Ioniq"];
+    const validValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const validInverseValues = ["1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "1/9"];
 
-    renderCriteriaList();
-    renderTable('criteria-matrix', 'Критерії', criteria);
+    const criteriaList = $('#criteria-list');
+    const matrixContainer = $('#matrix-container');
+
+    renderData();
 
     $(document).on('click', '.criteria-delete', (e) => {
         let index = $(e.currentTarget).data('index') - 1;
         criteria.splice(index, 1);
-        renderCriteriaList();
+        renderData();
     });
 
     $(document).on('change', '.criteria-input', (e) => {
         let self = $(e.currentTarget);
         let index = self.data('index') - 1;
         criteria[index] = self.val();
+        renderData();
+    });
+
+    $(document).on('change', '.matrix-cell', (e) => {
+        let self = $(e.currentTarget);
+        let opposite = self.data('opposite');
+        let tableId = self.data('table');
+        let value = self.val();
+
+        if (validValues.indexOf(value) === -1 && validInverseValues.indexOf(value) === -1) {
+            self.addClass('uk-form-danger');
+        } else {
+            self.removeClass('uk-form-danger');
+            let cell = $(`#${tableId}-cell-${opposite}`);
+            let oppositeValue = validInverseValues[validValues.indexOf(value)];
+
+            if (validInverseValues.indexOf(value) !== -1) {
+                oppositeValue = validValues[validInverseValues.indexOf(value)];
+            }
+
+            cell.val(oppositeValue);
+        }
+
     });
 
     $('#add-criteria').click(() => {
         criteria.push('');
-        renderCriteriaList()
+        renderData()
     });
+
+    function renderData() {
+        renderCriteriaList();
+        renderTables();
+    }
 
     function renderCriteriaList() {
         let html = '';
@@ -43,6 +75,32 @@ $(document).ready(() => {
         criteriaList.html(html);
     }
 
+    function renderTables() {
+        // render criteria
+        matrixContainer.html(
+        `<div class="uk-container">
+            <h2>Оцініть відносні переваги критеріїв</h2>
+            <div class="uk-overflow-auto" uk-margin>
+                <table id="criteria-matrix" class="uk-table uk-table-responsive">
+                </table>
+            </div>
+        </div>`);
+        renderTable('criteria-matrix', 'Критерії', criteria);
+
+        // render alternatives per criteria tables
+        for(let i=0; i<criteria.length; i++) {
+            matrixContainer.append(
+            `<div class="uk-container">
+                <h2>Оцініть відносні переваги автомобілів за критерієм "${criteria[i]}"</h2>
+                <div class="uk-overflow-auto" uk-margin>
+                    <table id="criteria${i}-matrix" class="uk-table uk-table-responsive">
+                    </table>
+                </div>
+            </div>`);
+            renderTable(`criteria${i}-matrix`, criteria[i], cars);
+        }
+    }
+
     function renderTable(tableId, tableTitle, titles) {
         let table = $(`#${tableId}`);
         let row, cell;
@@ -61,13 +119,21 @@ $(document).ready(() => {
         for(let i=0; i<titles.length; i++){
             row = $( '<tr />' );
             table.append( row );
-            cell = $('<th>'+titles[i]+'</th>')
+            cell = $('<th>'+titles[i]+'</th>');
             row.append( cell );
 
             for(let j=0; j<titles.length; j++){
+                let disabled = '';
+                let value = '';
+                if (titles[i] === titles[j]) {
+                    disabled = 'disabled';
+                    value = 1;
+                }
+                let opposite = j + '' + i;
+                let cellId = i + '' + j;
                 cell = $('<td>' +
-                    '<input class="uk-input uk-form-width-xsmall" type="text" placeholder="" value="1" max="9" min="0" disabled>'
-                    + '</td>')
+                    `<input id="${tableId}-cell-${cellId}" class="uk-input uk-form-width-xsmall matrix-cell" type="text" data-opposite="${opposite}" data-table="${tableId}" placeholder="" value="${value}" max="9" min="0"` + disabled + '>'
+                    + '</td>');
                 row.append( cell );
             }
 
