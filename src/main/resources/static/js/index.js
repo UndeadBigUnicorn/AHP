a09686157d84bd46087534a36ead0a856d949b37
UndeadBigUnicorn <kravchel16@gmail.com>
2020-03-21 21:52:12 +0200

    fix typo

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
 
-    const criteria = ["Ціна", "Запас ходу", "Швидкість", "Зарядка", "Вмісткість", "Комфорт"];
+    const criteria = ["Ціна", "Запас ходу", "Швидкість", "Зарядка", "Місткість", "Комфорт"];
     const cars = ["Tesla Model 3", "Nissan Leaf", "VW e-Golf", "Hyundai Ioniq"];
     const validValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     const validInverseValues = ["1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "1/9"];
